f06a270f2da24c2207a221ad2472ebe6148ad484
Muhammet Dobaoğlu <106467744+Muhammet-Dobaoglu@users.noreply.github.com>
2022-10-08 22:45:27 +0300

    Add files via upload

---
+++ Homework-2/Program.cs
@@ -0,0 +1,30 @@
+﻿#region Asal Sayı
+
+Console.Write("Sayıyı giriniz:");
+
+int sayi = int.Parse(Console.ReadLine());
+bool asaldegil = false;  
+
+if (sayi <= 0) 
+    Console.WriteLine("Asal sayılar sıfırdan küçük olmaz...");
+
+
+else
+{
+    for (int i = 2; i < sayi; i++)
+    { 
+        if (sayi % i == 0)
+        {
+            asaldegil = true;     
+        }
+    }
+
+    if (asaldegil==true)  
+        Console.WriteLine(sayi + " Asal Sayı Değildir..."); 
+    else
+        Console.WriteLine("Asal Sayıdır...");    
+}
+
+Console.ReadLine();
+
+#endregion
